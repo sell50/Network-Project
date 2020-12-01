fda37d1940c501581e0d723256327dbab226704f
Kat <pacek@uwindsor.ca>
2020-11-30 19:22:04 -0500

    change filename and add class

---
+++ detectonlineip.py
@@ -0,0 +1,22 @@
+import socket
+import os
+
+#This program answers the first one-to-one job 
+class detectonlineip(object):
+
+    def detect(self, port):
+
+        targetip = input('Enter the host to be scanned: ')
+        targetaddress = socket.gethostbyname(targetip)
+
+        print('Looking to see if host is online...')
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Create a TCP/IP socket
+
+        rep = os.system('ping ' + targetaddress)
+        if rep == 0:
+            print('')
+            print(targetaddress, 'is online!')
+        else:
+            print('')
+            print(targetaddress, 'is not online!')
