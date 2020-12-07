328613e127b168cd2aa77efaf61781969add403c
Brett Shepley <67429536+BrettShepley11@users.noreply.github.com>
2020-12-07 15:47:28 -0500

    Create FileRecord.py

---
+++ Final Project/FileRecord.py
@@ -0,0 +1,17 @@
+class FileRecord(object):
+  
+  def __init__(self):
+  
+  
+  def recordOutput(self):
+    try:
+      outputFile = open("ClientOutput.txt")
+    except IOError:
+    
+    finally:
+  
+  def updateJobListBackup(self):
+  
+  
+  def readJobListBack(self):
+  
