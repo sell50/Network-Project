224b01b5f481685453199b93bb3a34cb86141eec
Brett Shepley <67429536+BrettShepley11@users.noreply.github.com>
2020-12-10 02:12:48 -0500

    Two different functions created and commented
    First, recordOutput function was created for the Server so it could log each completed job and gather it's output in one location. Second, updateJobListBackup function was created incase the Server crashes, each time the Job List is updated on the Server end it will update the text file. When the Server comes back online it will read the text file and gather all the previous jobs before the crash occurred.

--- Final Project/FileRecord.py
+++ Final Project/FileRecord.py
@@ -1,13 +1,25 @@
 class FileRecord(object):
 
     def recordOutput(self, clientOutput):
-
+        """
+        Input: clientOutput (String)
+        Description: This function checks for 'ClientOutput.txt', if not found
+                    it will create this file and append the given input to this file.
+                    After all is done, the stream to the file is closed ensuring the 
+                    file gets saved
+        """
         outputFile = open("ClientOutput.txt", 'a+')
         outputFile.write(clientOutput)
         outputFile.close()
 
     def updateJobListBackup(self, Job):
-        
+        """
+        Input: Job (String)
+        Description: This function checks for 'JobBackup.txt', if not found
+                    it will create this file and append the given input to this file.
+                    After all is done, the stream to the file is closed ensuring the 
+                    file gets saved
+        """
         jobBackup = open("JobBackup.txt", 'a+')
         jobBackup.write(Job)
         jobBackup.close()
