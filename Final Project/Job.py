35442f448fd07be108c9f13620d7a6fef79d0ce3
Brett Shepley <67429536+BrettShepley11@users.noreply.github.com>
2020-12-10 02:27:33 -0500

    Minor Bug Fix and Comments Added

--- Final Project/Job.py
+++ Final Project/Job.py
@@ -2,17 +2,25 @@
 class Job(object):
 
     def __init__(self, jobCreator, jobName, numOfSeekers, targetIP, targetPort):
-
+        """
+        Input: <String> <String> <String> <String> <String> 
+        Description: This Constructor creates a Job object with all the below 
+                    attributes which can be accessed by the Server
+        """
         self.JobCreator = jobCreator
         self.JobName = jobName
         self.NumOfSeekers = numOfSeekers
         self.targetIP = targetIP
         self.targetPort = targetPort
         self.jobParameters = [jobCreator, jobName, numOfSeekers, targetIP, targetPort]
-        self.FullJob = jobCreator+" "+" "+jobName+" "+" "+numOfSeekers
+        self.FullJob = jobCreator+" "+jobName+" "+numOfSeekers+" "+targetIP+" "targetPort
         self.JobSeekerList = []
 
     def __iter__(self):
+        """
+        Input: <null> 
+        Description: This function is used to iterate through the below attributes
+        """
         yield self.JobCreator
         yield self.JobName
         yield self.NumOfSeekers
@@ -20,6 +28,10 @@ class Job(object):
         yield self.targetPort
 
     def addSeekerList(self, SeekerName):
+        """
+        Input: <String> 
+        Description: This function will add the given input to a List of Job Seekers
+        """
         self.JobSeekerList.append(SeekerName)
 
     '''
